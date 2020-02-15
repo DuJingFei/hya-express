f4154118a75b7a0e5d49b4ac2fab1e920c8612cc
jingfei_du <740677751@qq.com>
2020-02-15 23:07:39 +0800

    contact-item

--- routes/contact.js
+++ routes/contact.js
@@ -25,6 +25,18 @@ router.get('/contact/list', function(req, res, next) {
   })
 });
 
+// 根据 classfy 获取
+router.get('/contact/:id', function(req, res, next) {
+  let id = req.params && req.params.id
+  validLogin(req).then(result => {
+    return geContactItem(id).then(listData => {
+      res.json(new SuccessModel(listData))
+    }).catch(err => {
+      
+    })
+  })
+});
+
 // 新增type
 router.post('/contact/add', function(req, res, next) {
   let params = {name, parentId , content , classfy, orderIndex} = req.body;
